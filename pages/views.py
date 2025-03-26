deac8346eb470b23eb929f5091bcde79b3810646
Juned-the-programmer <davdajuned7698@gmail.com>
2025-03-26 11:52:18 +0530

    Added Backup Route

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.http import FileResponse
+from django.conf import settings
+import os
 
 # Create your views here.
 def index(request):
     return render(request, 'pages/index.html')
+
+def backup_database(request):
+    db_path = os.path.join(settings.BASE_DIR, "db.sqlite3")
+    return FileResponse(open(db_path, 'rb'), as_attachment=True, filename="db.sqlite3")
